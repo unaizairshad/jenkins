cbba3e2577a1db44e12524f1f9f54da8a25f7ed1
ahamauna <ahamauna@W5CG81966N9.bbrouter>
2022-10-17 11:04:45 +0530

    spring-using-jenkins pushed

---
+++ spring-using-jenkins/src/main/java/com/example/demo/controllers/StudentControllers.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentControllers {
+	
+	@GetMapping("/api/v1/students")
+	public List<String> getNames(){
+		return Arrays.asList("Ram","Shyam");
+	}
+
+}
